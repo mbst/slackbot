7336a2c64e57338e235f7271c66fd90fa29bc11d
Jason Howmans <jasonhowmans@me.com>
2014-10-08 15:28:39 +0100

    Getting issue from jira api

--- bots/jira.js
+++ bots/jira.js
@@ -1,21 +1,29 @@
 'use strict';
-var express = require('express'),
-    logger = require('../lib/logger').jirabot,
-    router = express.Router(),
-    dispatcher = require('../lib/dispatcher');
+var express     = require('express'),
+    logger      = require('../lib/logger').jirabot,
+    router      = express.Router(),
+    dispatcher  = require('../lib/dispatcher'),
+    http        = require('http'),
+    _           = require('lodash');
 
 //  used for taking the request body and filtering it
 //  to output a message string
 //  @param body {object}
 //  @returns {string} slack message or null
 function formatter(body) {
-    if ( typeof body !== 'object' ) return null;
+    if (!_.isObject(body)) {
+        logger.error('formatter(): body argument must be object'); 
+        return;
+    }
     var output = [],
         ev          = body.webhookEvent,
         user        = body.user,
         issue       = body.issue,
         changelog   = body.changelog.items,
         resolution  = issue.fields.resolution || null;
+
+    // get info about the parent issue
+    getIssue(body.key);
 
     // construct the response 
     output.push(user.displayName);
@@ -37,12 +45,28 @@ function formatter(body) {
     return output.join(' ');
 }
 
+//  used for requesting the issue from jira
+//
+//  @param issueID {string} the id of the issue. eg: `MBST-9704`
+function getIssue(issueID) {
+    if (!_.isString(issueID)) {
+        logger.error('getIssue(): issueID argument must be a string'); 
+        return;
+    }
+    var _endpoint = 'jira.metabroadcast.com/rest/api/2/issue/'+issueID;
+
+    // make the request to jira, and return project info
+    http.get(endpoint, function(req, res) {
+        var _body = req.body || null;
+        console.log(_body);
+    }).end();
+}
+
 // listen for incoming hooks from jira
 router.route('/').post( function(req, res) {
-    var body = req.body || null;
-    console.log(body);
-    logger.log(body)
-    var response = formatter(body);
+    var _body = req.body || null;
+    var response = formatter(_body);
+
     dispatcher.send('#anything-else', response, {
         username: 'Jira',
         color: '#053663',
