6f97321f89d01f55c73405e710d695b1513dc696
Jason Howmans <jasonhowmans@me.com>
2014-10-10 15:24:16 +0100

    Fix distinction between issues and features

--- bots/jira.js
+++ bots/jira.js
@@ -8,46 +8,57 @@ var express     = require('express'),
     _           = require('lodash'),
     q           = require('q');
 
-//  used for taking the request body and filtering it
-//  to output a message string
-//  @param taskdata {object}
-//  @param featuredata {object}
+//  Used for taking the request body and filtering it to output a
+//  message string
+//
+//  @param taskdata {object} 
+//  @param featuredata {object} omitting this causes the data 
+//         to be treated as a parent feature, not an issue
 //  @returns {string} slack message or null
 function formatter(taskdata, featuredata) {
-    if (!_.isObject(taskdata) || !_.isObject(featuredata)) {
-        logger.error('formatter(): taskdata and featuredata arguments must be objects'); 
-        return;
+    if (!_.isObject(taskdata)) {
+        logger.error('formatter(): taskdata argument must be a object'); 
+        return; 
     }
     var output      = [],
+        isFeature   = (_.isObject(featuredata))? false : true,
         ev          = taskdata.webhookEvent,
         user        = taskdata.user,
         issue       = taskdata.issue,
-        changelog   = taskdata.changelog.items,
         resolution  = issue.fields.resolution || null,
-        browseURL   = 'http://jira.metabroadcast.com/browse/';
+        browseURL   = 'http://jira.metabroadcast.com/browse/',
+        wording     = {};
 
     // construct the response 
+    wording.type = (isFeature)? 'feature' : 'issue';
     output.push(user.displayName);
     if ( ev === 'jira:issue_created' ) {
-        output.push('has created issue');
+        output.push('has created');
     } else
     if ( ev === 'jira:issue_deleted' ) {
-        output.push('has deleted issue');
+        output.push('has deleted');
     } else
     if ( ev === 'jira:issue_updated' ) {
         if ( resolution === null ) {
-            output.push('has updated issue');
+            output.push('has updated');
         }else{
-            output.push('has resolved issue');
+            output.push('has resolved');
         }
     }
+    output.push(wording.type);
     output.push('<'+browseURL+issue.key+'|'+issue.fields.summary+'>');
-    output.push('in the feature <'+browseURL+featuredata.key+'|'+featuredata.fields.summary+'>');
+    if (isFeature) {
+        output.push('in the feature');
+        output.push('<'+browseURL+featuredata.key+'|'+featuredata.fields.summary+'>');
+    }
     return output.join(' ');
 }
 
 
 //  Used for making requests to jira
+//
+//  @param path {string}
+//  @param type {string} 'GET' by default
 function jiraRequestWithAuth(path, type) {
     if (!_.isString(path)) {
         logger.error('jiraRequestWithAuth(): path argument must be string'); 
@@ -100,17 +111,32 @@ function getIssueInfo(issueID) {
 
 // listen for incoming hooks from jira
 router.route('/').post( function(req, res) {
-    var taskdata = req.body || null;
-    console.log(taskdata);
-    getIssueInfo(taskdata.issue.fields.customfield_10400).then(function(featuredata) {
-        var response = formatter(taskdata, featuredata);
+    var taskdata = req.body || null;    
+
+    // determine if this request is for a top level
+    // feature or a child issue
+    if (_.isString(taskdata.issue.fields.customfield_10400)) {
+        // send as issue
+        var parent_issue = taskdata.issue.fields.customfield_10400;
+        getIssueInfo(parent_issue).then(function(featuredata) {
+            var response = formatter(taskdata, featuredata);
+            dispatcher.send('#anything-else', response, {
+                username: 'Jira',
+                color: '#053663',
+                icon_url: 'https://confluence.atlassian.com/download/attachments/284366955/JIRA050?version=1&modificationDate=1336700125538&api=v2'
+            })
+            res.end();
+        });
+    }else{ 
+        // send as feature
+        var response = formatter(taskdata);
         dispatcher.send('#anything-else', response, {
             username: 'Jira',
             color: '#053663',
             icon_url: 'https://confluence.atlassian.com/download/attachments/284366955/JIRA050?version=1&modificationDate=1336700125538&api=v2'
         })
         res.end();
-    });
+    }
 });
 
 module.exports = router;
