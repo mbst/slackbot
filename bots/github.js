552d0a46edb9d460ef0a8428f76c1bf07906667e
Jason Howmans <jasonhowmans@me.com>
2014-10-27 19:02:42 +0000

    Improvements to dispatcher && github hooks now trigger messages

--- bots/github.js
+++ bots/github.js
@@ -7,18 +7,42 @@ var express     = require('express'),
     _           = require('lodash'),
     q           = require('q');
 
+//  For dealing with a pull request and dispatching to the correct chat
+//
+//  @param body {object} the body of the request
+//
+function handle_pull_request(body) {
+    var _message_options = {
+        username: 'Github',
+        color: '#000',
+        icon_url: 'https://octodex.github.com/images/bouncercat.png'
+    }
+
+    if ("pull_request" in body) {
+        var _pr = body.pull_request;
+        var message = new dispatcher('#anything-else', _message_options);
+        message.write(_pr.user.login)
+               .write('has made a pull request')
+               .link(_pr.title, _pr.html_url)
+               .send();
+    }
+}
+
 router.route('/').post(function(req, res, next) {
     var _body = req.body || null;
     var _event = req.headers['x-github-event'] || null;
 
-    // end the response here when _event or _body are empty
-    if (!!_body || !!_event) {
+    // end the response here if any of the request data is missing
+    if (!_body || !_event) {
         res.end('not enough data to continue');
         next();
         return;
     }
 
-    
+    if (_event === 'pull_request') {
+        handle_pull_request(_body);
+    }
+
     res.end('YaY');
     next();
 });
