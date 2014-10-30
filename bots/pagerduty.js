49df81d71bfd6f54b54acbf3e57e32f813c4e669
Jason Howmans <jasonhowmans@me.com>
2014-10-30 18:12:12 +0000

    Pagerdutybot

---
+++ bots/pagerduty.js
@@ -0,0 +1,22 @@
+'use strict';
+var express         = require('express'),
+    common          = require('../lib/common'),
+    logger          = require('../lib/logger').pagerdutybot,
+    router          = express.Router(),
+    dispatcher      = require('../lib/dispatcher'),
+    _               = require('lodash'),
+    Q               = require('q');
+
+router.post('/', function(req, res) {
+    var _body = req.body || null;
+
+    var _message_options = {
+        username: 'Pagerduty',
+        color: '#47BA04',
+        icon_url: 'https://pbs.twimg.com/profile_images/482648331181490177/4X_QI2Vu_400x400.png'
+    };
+    var message = new dispatcher('#support', _message_options);
+    console.log(_body);
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use('/test', require('./bots/test'));
 app.use('/webhooks/jira', require('./bots/jira'));
 app.use('/webhooks/github', require('./bots/github'));
 app.use('/webhooks/bitbucket', require('./bots/bitbucket'));
+app.use('/webhooks/pagerduty', require('./bots/pagerduty'));
 
 // boot the server
 console.log('Bot ready @ http://dev.mbst.tv:'+common.port);
