9313c88cc4c27ab3039e3ccd57da813e4091cd77
Jason Howmans <jason@metabroadcast.com>
2015-05-29 11:32:13 +0100

    Better json output on pgrduty log.

--- bots/pagerduty/boot.js
+++ bots/pagerduty/boot.js
@@ -8,7 +8,7 @@ var router     = express.Router();
 router.post('/', function(req, res) {
   var _body = req.body || null;
   console.log('pagerduty');
-  console.log(req.body);
+  console.log(JSON.stringify(req.body));
   console.log('/pagerduty');
   pagerduty.parseRequest(_body, pagerduty.sendMessage);
   res.end();
