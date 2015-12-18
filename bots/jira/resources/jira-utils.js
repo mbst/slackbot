5b89a0602492339057d2e271bd5c99eb612f9f2d
Devo <devo@ip-10-0-1-19.eu-west-1.compute.internal>
2015-12-18 16:30:22 +0000

    Removed conditional to always see what is being returned by webhook.

--- bots/jira/resources/jira-utils.js
+++ bots/jira/resources/jira-utils.js
@@ -33,11 +33,12 @@ module.exports.formatter = function formatter (taskdata, featuredata) {
   }
 
   try {
-    if (action === 'updated' && taskdata.changelog.items[0]['toString'] === 'Resolved') {
-      action = 'resolved';
-    }
+    // if (action === 'updated' && taskdata.changelog.items[0]['toString'] === 'Resolved') {
+    //   action = 'resolved';
+    // }
+    action = taskdata.changelog.items[0]['toString'];
   } catch(e) {
-
+    // action = 'error';
   }
 
   if (_.has(taskdata, 'user')) {
