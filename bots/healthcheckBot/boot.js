7dc22a4afde0dbe678a331eede6f06b60a30905b
Thomas McDevitt <devo@metabroadcast.com>
2016-02-09 17:55:32 +0000

    Corrected some logic in healthcheckBot that was causing messages not to get posted.

--- bots/healthcheckBot/boot.js
+++ bots/healthcheckBot/boot.js
@@ -13,14 +13,7 @@ var channel = '#testing-bots';
 router.route('/').post(function(req, res) {
 
   var body = req.body;
-  var chatName = body.channel_name;
   var healthbotToken = 'eV03fse45Zw6YUjijHFgcD1p';
-
-  if (chatName !== 'anything-else') {
-    logger.warn('AbedBot can only be invoked in #anything-else');
-    res.end();
-    return;
-  }
 
   if (body.token !== healthbotToken) {
     logger.warn('Invalid access token supplied.');
@@ -33,7 +26,8 @@ router.route('/').post(function(req, res) {
 
   msg
     .chat(channel)
-    .write('Automated Slackbot health report: ' + time)
+    .write('(' + time + ') - Slackbot is OK!')
+    .a()
     .avatar('http://4.bp.blogspot.com/-rrTvc825Oe0/Tw1rc1VTrcI/AAAAAAAAAEM/VV6uDl4LJR8/s200/Plus_Sign_Green.jpg')
     .send();
 
